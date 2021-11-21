93fb8faa47ae6f70e5ff81979e6cdfe316bedcb8
Pavel Zubkou <pavel.zubkou@gmail.com>
2021-11-22 01:03:46 +0300

    Initial commit

--- _resouce/js/script.js
+++ _resouce/js/script.js
@@ -1,42 +1,42 @@
-//デフォルトアイコンパス
 L.Icon.Default.imagePath = 'img/icon/';
 
-//MIERUNE Color読み込み
-const m_color = new L.tileLayer('https://tile.mierune.co.jp/mierune/{z}/{x}/{y}.png', {
-    attribution: "Maptiles by <a href='http://mierune.co.jp/' target='_blank'>MIERUNE</a>, under CC BY. Data by <a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors, under ODbL."
-});
+const lat = 53.68;
+const lng = 23.81;
 
-//MIERUNE MONO読み込み
-const m_mono = new L.tileLayer('https://tile.mierune.co.jp/mierune_mono/{z}/{x}/{y}.png', {
-    attribution: "Maptiles by <a href='http://mierune.co.jp/' target='_blank'>MIERUNE</a>, under CC BY. Data by <a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors, under ODbL."
-});
+const maps = {};
 
-//経緯度設定
-const lat = 35.681;
-const lng = 139.763;
+const createMap = (letter, tiles, name) => {
+    const map = L.map(`map-${letter}`, {
+        center: [lat, lng],
+        zoom: 14,
+        zoomControl: true,
+        layers: [
+            new L.tileLayer(tiles, {
+                attribution: name
+            }),
+            L.centerCross(),
+        ]
+    });
 
-//MAP読み込み
-const map = L.map('map', {
-    center: [lat, lng],
-    zoom: 14,
-    zoomControl: true,
-    layers: [m_mono]
-});
+    maps[letter] = map;
 
-//背景レイヤ
-const Map_BaseLayer = {
-    "MIERUNE Color": m_color,
-    "MIERUNE MONO": m_mono
-};
+    return map;
+}
 
-//レイヤ設定
-L.control.layers(
-    Map_BaseLayer,
-    null
-).addTo(map);
+const mapA = createMap('a', 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 'OpenStreetMap');
+createMap('b', 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', 'Esri');
+createMap('c', 'https://mapwarper.net/maps/tile/60517/{z}/{x}/{y}.png', '1941-07-24');
+createMap('d', 'https://mapwarper.net/mosaics/tile/1471/{z}/{x}/{y}.png', '1944-07-19');
+createMap('e', 'https://mapwarper.net/mosaics/tile/1469/{z}/{x}/{y}.png', 'GX929 A');
+createMap('f', 'https://mapwarper.net/mosaics/tile/1468/{z}/{x}/{y}.png', 'GX929 B');
+createMap('g', 'https://mapwarper.net/mosaics/tile/1467/{z}/{x}/{y}.png', '1944-09-16');
+createMap('h', 'https://mapwarper.net/mosaics/tile/1472/{z}/{x}/{y}.png', '1944-12-20');
 
-//スケール設定
 L.control.scale({
     imperial: false,
     maxWidth: 300
-}).addTo(map);
+}).addTo(mapA);
+
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+letters.forEach(x => letters.forEach(y => x !== y && maps[x].sync(maps[y])));
--- _resouce/main.js
+++ _resouce/main.js
@@ -1,6 +1,6 @@
-// CSS一式を読み込んでパッケージ
 import "leaflet/dist/leaflet.css";
 import "./css/style.css";
 
-// JS一式を読み込んでパッケージ
+import 'leaflet.sync';
+import 'leaflet-center-cross';
 import './js/script.js';
